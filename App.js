6a4773f8e7a124c63eb82080c641dc27a7f62a7f
JyoDesai <jyotidesai01@gmail.com>
2025-01-03 00:15:53 +0000

    Added updated code

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 
 // const heading = React.createElement("h1", {}, "Namste React");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
